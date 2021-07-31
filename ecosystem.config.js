cb15668eb4af13849b2865990cab2ec86915746b
acer_pc <wang24118@qq.com>
2021-07-31 13:36:37 +0800

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [{
+    name: "app1",
+    script: "./build/app.js",
+    watch: ["./build"],
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    max_restarts: 10
+  }]
+}
