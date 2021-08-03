f46f18074facf992efcad976734cd6313b506217
aliecsy5 <wang24118@qq.com>
2021-08-03 23:15:37 +0800

    update pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "app1",
     script: "./build/app.js",
-    watch: ["./build"],
+    watch: ["build"],
     log_date_format: "YYYY-MM-DD HH:mm:ss Z",
     max_restarts: 10
   }]
